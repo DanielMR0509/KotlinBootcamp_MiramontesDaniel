9ef9796475846b218f6f0e704754234bc7940550
DanielMR0509 <daniel.miramontes252801@potros.itson.edu.mx>
2026-01-31 18:35:23 -0700

    Primer ejercicio modulo 2-2.6

---
+++ src/modulo2-2.6.kt
@@ -0,0 +1,28 @@
+fun main() {
+    // ejercicio 1 del modulo 2-2.6
+    2.plus(71).plus(233).minus(13).div(30).plus(1)
+
+    // ejercicio 2
+    var rainbowColor = "red"
+    rainbowColor = "blue"
+    val blackColor = "black"
+    blackColor = "white" // valor que no se puede cambiar
+
+    // ejercicio 3
+    var greenColor = null
+    var blueColor: Int? = null
+
+    // ejercicio 4
+
+        // forma 1
+    listOf(null,null)
+
+        // forma 2
+    [null, null]
+
+    var list: List, = listOf(null, null)
+    var list2:List,? = null
+
+    // ejercicio 5
+    println(nullTest?.inc() ?:0)
+}
