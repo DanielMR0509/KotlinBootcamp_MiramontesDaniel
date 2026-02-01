4352afa2b84680860dee7c7e5f8713f0f02f9cf1
DanielMR0509 <daniel.miramontes252801@potros.itson.edu.mx>
2026-01-31 20:30:08 -0700

    Ejercicio modulo 3-3.11

---
+++ src/modulo3-3.11.kt
@@ -0,0 +1,18 @@
+// ejercicio modulo 3-3.11
+
+fun canAddFish(
+    tankSize: Double,
+    currentFish: List<Int>,
+    fishSize: Int = 2,
+    hasDecorations: Boolean = true
+): Boolean {
+    val maxCapacity = if (hasDecorations) tankSize * 0.8 else tankSize
+    return (currentFish.sum() + fishSize) <= maxCapacity
+}
+
+fun main() {
+    println(canAddFish(10.0, listOf(3, 3, 3)))
+    println(canAddFish(8.0, listOf(2, 2, 2), hasDecorations = false))
+    println(canAddFish(9.0, listOf(1, 1, 3), 3))
+    println(canAddFish(10.0, listOf(), 7, true))
+}
