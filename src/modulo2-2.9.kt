8d08165db6c512b90ffad3eb008d7b79a6f78d1c
DanielMR0509 <daniel.miramontes252801@potros.itson.edu.mx>
2026-01-31 18:47:44 -0700

    Ejercicios modulo 2-2.9

---
+++ src/modulo2-2.9.kt
@@ -0,0 +1,14 @@
+fun main() {
+    // ejercicio 1 del modulo 2-2.9
+    val trout = "trout"
+    var haddock = "haddock"
+    var snapper = "snapper"
+    println("Me gusta el $haddok y el $trout pero el $snapper no.")
+
+    // ejercicio 2
+    when(fishName.length) {
+        0 -> println("Error: name cant be empty")
+        in 3..12 -> println("Good fish name")
+        else -> println("OK fish name")
+    }
+}
